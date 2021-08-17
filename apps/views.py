18e76a8d8cd5ef5bdfb281c510a5b62605bae1ab
Konstantin <balashov-92@mail.ru>
2021-08-17 20:41:32 +0300

    wip: added model App

--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from apps.models import AppLog
+from apps.serializers import AppLogSerializer
+
+
+class CreateAppLog(CreateAPIView):
+    serializer_class = AppLogSerializer
+
+    def get_queryset(self):
+        return AppLog.objects.filter(app_id=self.request.data.get('app_id'))
+
