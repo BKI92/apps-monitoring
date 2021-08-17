45b27bada99ff13acc9c0e56a9f1dbe4c21e56b2
Konstantin <balashov-92@mail.ru>
2021-08-17 20:39:14 +0300

    wip: added model App

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class App(models.Model):
+    app_id = models.CharField(max_length=200)
+
+
+class AppLog(models.Model):
+    app_id = models.CharField(max_length=200)
+    message = models.CharField(max_length=400, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'APP_ID_{self.app_id} LOG_{self.pk}'
