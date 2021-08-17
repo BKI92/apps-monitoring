c3ea4bafc1e4647c7a041e1e7d022fbf1e36e778
Konstantin <balashov-92@mail.ru>
2021-08-17 20:39:01 +0300

    wip: added model App

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import AppLog, App
 
-# Register your models here.
+
+@admin.register(AppLog)
+class AppLogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'app_id', 'created')
+    search_fields = ('id', 'app_id', 'created')
+    list_filter = ('created',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(App)
+class AppLogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'app_id', )
+    search_fields = ('id', 'app_id', )
+    empty_value_display = '-пусто-'
